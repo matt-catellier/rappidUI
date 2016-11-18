48e379af9cf782c0d8b6b2d951b860fcb40d0711
Matt Catellier <mattcatellier@gmail.com>
2016-11-18 15:58:06 -0800

    Completed task 1

--- KitchenSink/app.js
+++ KitchenSink/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cors = require('cors');
 var bodyParser = require('body-parser');
-var Data = require('./data');
+var Data = require('./data2');
 var app = express();
 
 app.use(express.static(__dirname));
